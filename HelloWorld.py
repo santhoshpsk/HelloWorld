6ea3d3589ede5b8f735e580387b7ab3da6253524
santhoshpsk <santhoshpsk@outlook.com>
2019-04-02 20:05:44 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,5 @@
+import os
+API_TOKEN = os.getenv('API_TOKEN')
 print("Hello World")
 print("This is a test")
 print("This is a test1")
@@ -5,3 +7,4 @@ print("This is a test3")
 print("This is a test4")
 print("This is a test5")
 print("This is a test6")
+print("API token" + API_TOKEN)
